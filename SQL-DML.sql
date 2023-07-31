579404ab5e13ff315b62eaf9573810de91935f9d
easy <leege5664@gmail.com>
2023-07-31 13:50:12 +0900

    20230731

--- SQL-DML.sql
+++ SQL-DML.sql
@@ -98,3 +98,42 @@ SELECT DISTINCT employee_number, age FROM employee;
 # 검색 결과를 정렬할 때 사용
 # SELECT 컬럼 FROM 테이블명, WHERE 조건 ORDER BY 컬럼명 ASC | DESC, ...;
 SELECT * FROM employee ORDER BY employee_number DESC;
+
+# 연산자 (비교, 논리)
+SELECT * FROM bbq;
+SELECT DISTINCT 업소명 FROM bbq;
+SELECT * FROM bbq ORDER BY 업소명;
+SELECT * FROM bbq WHERE 업소명 = '교촌치킨';
+SELECT * FROM bbq WHERE 업소명 <> '비비큐';
+# = : 좌항이 우항과 같으면 true
+# != / <> : 좌항이 우항과 다르면 true
+
+# > : 좌항이 우항보다 크면 true
+# < : 좌항이 우항보다 작으면 true
+# >= : 좌항이 우항보다 크거나 같으면 true
+# <= : 좌항이 우항보다 작거나 같으면 true
+SELECT * FROM bbq WHERE 경도 > 35.3;
+SELECT * FROM bbq WHERE 업소명 > '닭썸' ORDER BY 업소명;
+
+UPDATE bbq SET 업소명 = null WHERE 업소명 = '비비큐';
+
+# <=> : 좌항과 우항이 모두 null일때 true 반환
+SELECT * FROM bbq WHERE 업소명 <=> null;
+
+# IS : 좌항과 우항이 같으면 true 반환 (teur, false, unknown, null)
+# IS NULL : 좌항이 NULL이면 true 반환
+SELECT * FROM bbq WHERE 업소명 IS NULL;
+SELECT * FROM bbq WHERE 업소명 IS TRUE ORDER BY 업소명;
+
+# IS NOT : 좌항이 우항과 다르면 true 반환 (teur, false, unknown, null)
+# IS NOT NULL : 좌항이 null이 아니면 true 반환
+SELECT * FROM bbq WHERE 업소명 IS NOT NULL;
+
+# BETWEEN min AND B : min 보다 크거나 같으면서 max 보다 작거나 같으면 true 반환
+# NOT BETWEEN min AND B : min 보다 작거나 같으면서 max보다 크면 true 반환
+SELECT * FROM bbq WHERE 위도 BETWEEN 129.027 AND 129.028;
+SELECT * FROM bbq WHERE 위도 NOT BETWEEN 129.027 AND 129.028;
+
+# IN() : 좌항이 인수로 전달받은 값중 하나라도 존재하면 true
+# NOT IN() : 좌항이 인수로 전달받은 값중 하나도 존재하지 않으면 true
+SELECT * FROM bbq WHERE 업소명 IN('BBQ개금점', 'BBQ반여점', '교촌치킨');
